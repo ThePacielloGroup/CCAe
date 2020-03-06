71bac0bc33f1f4becb50dc5fa7e4b930724d443c
Cédric <ctrevisan@paciellogroup.com>
2020-03-06 09:27:51 +0100

    New build for MacOS picker

--- src/picker/darwin/index.js
+++ src/picker/darwin/index.js
@@ -1,4 +1,4 @@
 var e=require("path")
 var r=require("child_process")
-exports.runColorPicker=()=>new Promise((o,i)=>(0,r.execFile)((0,e.join)(__dirname,"ColorPicker.app"),(e,r,t)=>{if(e)return i(e)
+exports.runColorPicker=()=>new Promise((o,i)=>(0,r.execFile)((0,e.join)(__dirname,"ColorPicker"),(e,r,t)=>{if(e)return i(e)
 o({possibleColorString:r})}))

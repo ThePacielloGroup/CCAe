83956c048e3c65c469b37201fca1a49221cc7447
Patrick H. Lauke <redux@splintered.co.uk>
2021-05-18 11:10:31 +0100

    Update contrast value based on most recent WCAG erratum
    See https://github.com/w3c/wcag/issues/360 and https://github.com/w3c/wcag/pull/1780

--- src/color/color.js
+++ src/color/color.js
@@ -259,7 +259,7 @@ Color.prototype = {
 		var lum = [];
 		for (var i = 0; i < rgb.length; i++) {
 			var chan = rgb[i] / 255;
-			lum[i] = (chan <= 0.03928) ? chan / 12.92 : Math.pow(((chan + 0.055) / 1.055), 2.4);
+			lum[i] = (chan <= 0.04045) ? chan / 12.92 : Math.pow(((chan + 0.055) / 1.055), 2.4);
 		}
 
 		return 0.2126 * lum[0] + 0.7152 * lum[1] + 0.0722 * lum[2];

d50b36bc2f94731629c011282d5b0f42821637c5
Ferllings <contact@ferllings.me>
2023-08-07 11:31:46 +0200

    [debug] Add logger and crash reporter

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-const { ipcMain, app } = require('electron')
+const { ipcMain, app, crashReporter } = require('electron')
+const log = require('electron-log/main')
+const path = require('node:path')
+// Clear logs every launch
+log.transports.file.getFile().clear()
+// initialize the logger for any renderer process
+log.initialize({ preload: true })
+// Redirect all console.* to logger
+Object.assign(console, log.functions)
+
+console.log(`app.getPath('logs')       = ${app.getPath('logs')}`)
+app.setPath('crashDumps', path.join(app.getPath('logs'), 'crashes'))
+crashReporter.start({ uploadToServer: false })
+
 const Store = require('electron-store');
 const { checkForUpdates, setUpdatesDisabled } = require('./update.js')
 const CCAController = require('./CCAcontroller')
